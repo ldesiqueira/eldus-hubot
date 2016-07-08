7b60785dd506599efe64b034839f24c37df712bf
ldesiqueira <eldusfelipus@gmail.com>
2016-07-08 16:16:30 -0700

    init

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
-include_recipe 'redis'
-include_recipe 'redis::server'
+#include_recipe 'redis'
+#include_recipe 'redis::server'
 package 'git'
+include_recipe 'eldus-hubot'
 javascript_runtime '0.12'
 %w{yo hubot}.each do |pkg|
   node_package pkg
@@ -16,7 +17,7 @@ git '/tmp/hubot' do
   revision 'master'
 end
 git '/tmp/jhubot' do
-  repository 'https://github.com:ldesiqueira/jobvite-hubot.git'
+  repository 'https://github.com/ldesiqueira/eldus-hubot.git'
   revision 'master'
 end
 
